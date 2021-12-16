c584ccf6b0b49ba3bdf584e008a9a9461f980ac4
Seth McCullough <seth@endevrs.dev>
2021-12-15 21:21:36 -0500

    Change login logout buttons to UD branding

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import {
   AppBar,
   Box,
   Container,
+  Typography,
   Toolbar,
   Button,
   LinearProgress,
+  Menu,
+  MenuItem,
 } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import type { User } from "./types";
 
 // Components
@@ -18,6 +22,7 @@ export default function App() {
   const [loading, setLoading] = useState<boolean>(false);
   const [user, setUser] = useState<User>();
   const [error, setError] = useState(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   useEffect(() => {
     (async () => {
@@ -80,16 +85,73 @@ export default function App() {
         <Toolbar>
           <Box sx={{ flexGrow: 1 }} />
           {user ? (
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={handleLogout}
-            >
-              Logout
-            </Button>
+            <>
+              <Button
+                size="large"
+                variant="contained"
+                color="secondary"
+                onClick={(e) => setAnchorEl(e.currentTarget)}
+                sx={{
+                  padding: 0.5,
+                  margin: 0.5,
+                  textTransform: "none",
+                }}
+              >
+                <img
+                  alt=""
+                  draggable="false"
+                  src="https://docs.unstoppabledomains.com/~/files/v0/b/gitbook-28427.appspot.com/o/assets%2F-MF1CaqLXCtpg4trwHjh%2F-MjjpJ6gM6yNltRf0-yc%2F-MjjudfnwJ7Gm_roYy5j%2Fdefault-icon.png?alt=media&amp;token=34f41fe8-5e26-45f2-ace2-6dffb113c031"
+                  style={{ height: 40 }}
+                />
+                <Typography
+                  variant="body1"
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    maxWidth: 150,
+                  }}
+                  noWrap
+                  ml={1}
+                >
+                  {user.address}
+                </Typography>
+                <KeyboardArrowDownIcon />
+              </Button>
+              <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={!!anchorEl}
+                onClose={() => {
+                  setAnchorEl(null);
+                }}
+              >
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </>
           ) : (
-            <Button variant="contained" color="secondary" onClick={handleLogin}>
-              Login with Unstoppable
+            <Button
+              color="secondary"
+              onClick={handleLogin}
+              sx={{
+                margin: 0,
+                padding: 0,
+                "&:hover": { filter: "brightness(0.85)" },
+              }}
+            >
+              <img
+                alt="unstoppable-login-button"
+                draggable="false"
+                src="https://docs.unstoppabledomains.com/~/files/v0/b/gitbook-28427.appspot.com/o/assets%2F-MF1CaqLXCtpg4trwHjh%2F-MjjpJ6gM6yNltRf0-yc%2F-MjjutLm4Kh7ikfH7Pi3%2Fpressed-button.png?alt=media&amp;token=e1ac2392-8d77-4a60-b7a7-fa9f65a34cb8"
+                style={{ height: 40 }}
+              />
             </Button>
           )}
         </Toolbar>
